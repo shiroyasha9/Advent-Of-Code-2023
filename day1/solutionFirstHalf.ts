ebc570db9f75b1e875e1651a52dc9ce3f447ac24
shiroyasha9 <mubinansari910@gmail.com>
2023-12-03 15:55:50 +0530

    Solution for first half of day1

---
+++ day1/solutionFirstHalf.ts
@@ -0,0 +1,26 @@
+const inputFile = Bun.file("./input.txt");
+const inputText = await inputFile.text();
+const splittedInput = inputText.split("\n");
+
+let sum = 0;
+
+for (let inputWord of splittedInput) {
+  let firstNumber: string | undefined;
+  let secondNumber: string | undefined;
+
+  for (let letter of inputWord) {
+    if (isNaN(parseInt(letter))) {
+      continue;
+    }
+    if (firstNumber === undefined) {
+      firstNumber = letter;
+    }
+    secondNumber = letter;
+  }
+
+  if (firstNumber !== undefined && secondNumber !== undefined) {
+    sum += parseInt(firstNumber + secondNumber);
+  }
+}
+
+console.log(sum);
